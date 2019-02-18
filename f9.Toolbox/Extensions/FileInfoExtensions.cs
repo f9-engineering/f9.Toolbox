0861ac64d729d866793b27fee59cca941e350d4f
Laurent De Plaen <l.deplaen@protonmail.com>
2019-02-18 14:41:03 +0100

    .

---
+++ f9.Toolbox/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace f9.Toolbox.Extensions
+{
+  public static class FileInfoExtensions
+  {
+    /// <summary>
+    /// Gets the relative path.
+    /// From : https://stackoverflow.com/a/703292/2939215
+    /// </summary>
+    /// <param name="file">The file.</param>
+    /// <param name="folder">The folder.</param>
+    /// <returns>The relative path to the file</returns>
+    public static string GetRelativePath(this FileInfo file, string folder)
+    {
+      Uri pathUri = new Uri(file.FullName);
+      // Folders must end in a slash
+      if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString()))
+      {
+        folder += Path.DirectorySeparatorChar;
+      }
+      Uri folderUri = new Uri(folder);
+      return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString().Replace('/', Path.DirectorySeparatorChar));
+    }
+  }
+}
---
+++ f9.Toolbox/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using log4net;
+
+namespace f9.Toolbox.Extensions
+{
+  public static class StringExtensions
+  {
+    private static readonly ILog m_Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+    public static string GetValidPathName(this string sequenceName)
+    {
+      var validName = sequenceName;
+      var invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+
+      foreach (var c in invalid)
+      {
+        validName = validName.Replace(c.ToString(), "");
+      }
+
+      if (validName != sequenceName)
+      {
+        m_Log.Warn("The sequence name " + sequenceName + " is not valid and has be changed to " + validName);
+      }
+
+      return validName;
+    }
+  }
+}
